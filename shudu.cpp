2de32f0ccfb0949f68ad735d49df5127d078a7bf
pyt <805926731@qq.com>
2023-06-26 20:15:29 +0800

    add shudu generate

--- shudu.cpp
+++ shudu.cpp
@@ -1,78 +1,52 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<memory.h>
-#include<math.h>
-#include<time.h>
-#include<string.h>
-#define MAX 999
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <random>
+#include <algorithm>
 
-struct Node{
-    Node *up,*down,*left,*right,*colPtr,*rowPtr;//指向row,col对应的行对象和列对象
-    int row_num;//记录行数，行专属(从1开始)
-    int col_elemCount;//记录该列元素个数，列专属
-};
+const int N = 9;
+const int EMPTY = 0;
 
-struct player{//玩家信息结点
-    int m ;
-    int s ;
-    char name[20];
-    int level ;
-    player* next;
-};
+// 函数原型
+void generateSudoku(std::vector<std::vector<int>>& grid);
+void writeSudokuToFile(const std::vector<std::vector<int>>& grid, const std::string& filename);
 
+// 生成数独终局
+void generateSudoku(std::vector<std::vector<int>>& grid) {
+    std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::shuffle(numbers.begin(), numbers.end(), rng);
 
-int row_size=593;//行数
-int col_size = 324;//列数
-int result[81];//存放结果行的栈
-int index = 0;//栈指针
-int sudoku[81] = {0};//存放数独
-int time_start = 0;
-int time_end = 0;//起始时间
-
-void init(Node* head){
-	head->left = head;
-	head->right = head;
-	head->up = head;
-	head->down = head;
-
-	for(int k = 0;k < row_size;k++){//创建行对象(头插)
-		Node* newNode = (Node*)malloc(sizeof(Node));
-		newNode->up = head;
-		newNode->down = head->down;
-		newNode->left = newNode;
-		newNode->right = newNode;
-		newNode->down->up = newNode;
-		head->down = newNode;
-		newNode->row_num = row_size-k;
-		newNode->col_elemCount = 0;//借用，作为标志
-	}
-}
-void init_col(Node* head){
-	/************初始化行列对象*******************/
-	for(int j = 0;j < col_size;j++){//创建列对象(头插)
-		Node* newNode = (Node*)malloc(sizeof(Node));
-		newNode->right = head->right;
-		newNode->left = head;
-		newNode->down = newNode;
-		newNode->up = newNode;
-		newNode->right->left = newNode;
-		head->right = newNode;
-		newNode->col_elemCount = 0;//列元素个数初始为0
-	}
+    for (int row = 0; row < N; row++) {
+        for (int col = 0; col < N; col++) {
+            grid[row][col] = numbers[(col + row * 3 + row / 3) % N];
+        }
+    }
 }
 
-int main(){
-    int player_res[81]={0};
-    int choice;
-    int** matrix;//存放数独的01矩阵
-    int* answer;//存放答案
-    player* easy;//容易难度排行
-    player* normal;//简单难度排行
-    player* hard;//困难难度排行
-    player info ;//玩家信息
+// 将数独终局写入文件
+void writeSudokuToFile(const std::vector<std::vector<int>>& grid, const std::string& filename) {
+    std::ofstream file(filename);
+    if (file.is_open()) {
+        for (const auto& row : grid) {
+            for (int num : row) {
+                file << num << " ";
+            }
+            file << "\n";
+        }
+        file.close();
+        std::cout << "数独终局已写入文件：" << filename << std::endl;
+    } else {
+        std::cout << "无法打开文件：" << filename << std::endl;
+    }
+}
 
-    Node* head =(Node*)malloc(sizeof(Node));
-    //初始化行列对象，建立十字双向循环链表
-    init(head);
-    init_col(head);
+int main() {
+    // 1. 生成数独终局
+    std::vector<std::vector<int>> grid(N, std::vector<int>(N, EMPTY));
+    generateSudoku(grid);
+    writeSudokuToFile(grid, "sudoku.txt");
+
+    return 0;
 }
